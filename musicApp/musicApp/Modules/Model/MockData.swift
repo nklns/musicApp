ecb9f8b4d515b81ee45b4026754010aa825a2832
nklns <145385401+nklns@users.noreply.github.com>
2024-10-16 14:17:38 +0500

    Поменять местами иконки

--- musicApp/musicApp/Modules/Model/MockData.swift
+++ musicApp/musicApp/Modules/Model/MockData.swift
@@ -11,7 +11,7 @@ import UIKit
 struct MockData {
     static let mockData: [CellModel] = [
         .init(image: UIImage(systemName: "lock.open")!, title: "Сегодня - бесплатный доступ", description: "Наслаждайся 1000+ музыки и подкастов"),
-        .init(image: UIImage(systemName: "checkmark.seal.fill")!, title: "День 5 - напоминание", description: "Мы отправим тебе уведомление, чтобы напомнить,\nкогда заканчивается пробный период"),
-        .init(image: UIImage(systemName: "bell.fill")!, title: "День 7 - окончание бесплатного периода", description: "Твоя подписка начнет действовать. Ты сможешь\nлегко отменить её после этого дня")
+        .init(image: UIImage(systemName: "bell.fill")!, title: "День 5 - напоминание", description: "Мы отправим тебе уведомление, чтобы напомнить,\nкогда заканчивается пробный период"),
+        .init(image: UIImage(systemName: "checkmark.seal.fill")!, title: "День 7 - окончание бесплатного периода", description: "Твоя подписка начнет действовать. Ты сможешь\nлегко отменить её после этого дня")
     ]
 }
